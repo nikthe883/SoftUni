b8a934019f7f4def475d5322df699e042067b364
nikthe883 <popmihaylov@gmail.com>
2022-05-29 14:05:49 +0300

    Some minor bug fixes

--- easy_life/renaming_projects.py
+++ easy_life/renaming_projects.py
@@ -3,7 +3,7 @@
 # app for renaming projects
 lesson_name = input()
 
-output = lesson_name.lower().replace(" ", "",1)
+output = lesson_name.lower().replace(" ", "", 1)
 final_output = output.replace(" ", "_")
 
 
