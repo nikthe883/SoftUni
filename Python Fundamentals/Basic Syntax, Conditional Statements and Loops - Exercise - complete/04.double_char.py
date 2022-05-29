f1f6e58175dbdb68ed194dade56d933427fc9336
nikthe883 <popmihaylov@gmail.com>
2022-05-29 14:05:33 +0300

    The requirements have change - app updated according to them.

--- Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise - complete/04.double_char.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise - complete/04.double_char.py
@@ -1,8 +1,9 @@
 word = input()
 
-while word != "END":
+while word != "End":
     double_char = ""
     if word != "SoftUni":
         for i in word:
             double_char += i*2
         print(double_char)
+    word = input()
