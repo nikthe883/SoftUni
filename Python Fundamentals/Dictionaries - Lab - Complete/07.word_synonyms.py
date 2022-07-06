9730318463c921de1588e0d2f10eaa79c18419f6
nikthe883 <82037390+nikthe883@users.noreply.github.com>
2022-07-06 19:22:37 +0300

    First Commit

--- Python Fundamentals/Dictionaries - Lab - Complete/07.word_synonyms.py
+++ Python Fundamentals/Dictionaries - Lab - Complete/07.word_synonyms.py
@@ -1,1 +1,12 @@
 n = int(input())
+sym = {}
+
+for i in range(n):
+    word = input()
+    synonym = input()
+    if word not in sym:
+        sym[word] = []
+    sym[word].append(synonym)
+
+for word in sym:
+    print(f"{word} - {', '.join(sym[word])}")
