141bd7e69f2381d4702c2a13fd6509cc02db9b2c
nikthe883 <82037390+nikthe883@users.noreply.github.com>
2022-10-19 00:39:01 +0300

    First commit

--- Python Advanced/Python Advanced Exams/Python Advanced Retake Exam - 14 April 2022/03.word_sorting.py
+++ Python Advanced/Python Advanced Exams/Python Advanced Retake Exam - 14 April 2022/03.word_sorting.py
@@ -1,26 +1,12 @@
 def words_sorting(*args):
-    my_dict = {}
+    words = {word: sum(map(ord, word)) for word in args}
+    result_str = []
 
-    for word in args:
-        letter_value = 0
-        for letter in word:
-            letter_value += ord(letter)
-        if word not in my_dict:
-            my_dict[word] = letter_value
+    if not sum(words.values()) % 2 == 0:
+        for word in sorted(words.items(), key=lambda x: -x[1]):
+            result_str.append(f"{word[0]} - {word[1]}")
+    else:
+        for word in sorted(words.items(), key=lambda x: x[0]):
+            result_str.append(f"{word[0]} - {word[1]}")
 
-    sum_values = 0
-    for v in my_dict.values():
-        sum_values += v
-
-    result = []
-    if sum_values % 2 != 0:
-        sorted_dict = sorted(my_dict.items(), key=lambda x: -x[1])
-        for i in sorted_dict:
-            result.append(f"{i[0]} - {i[1]}")
-
-    if sum_values % 2 == 0:
-        sorted_dict = sorted(my_dict.items(), key=lambda x: x[1])
-        for i in sorted_dict:
-            result.append(f"{i[0]} - {i[1]}")
-
-    return "\n".join(result)
+    return "\n".join(result_str)
