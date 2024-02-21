e9bd85859f68809c68a9f56b2501c3b931e871a7
nikthe883 <popmihaylov@gmail.com>
2024-02-21 17:54:51 +0200

    update

---
+++ Python OOP/Classes and Objects - Exercise/03/project/band.py
@@ -0,0 +1,17 @@
+
+
+class Band:
+    
+    albums = []
+
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def add_album(self, album):
+        pass
+
+    def remove_album(self,album_name):
+        pass
+
+    def details(self):
+        pass
--- Python OOP/Classes and Objects - Exercise/03/project/song.py
+++ Python OOP/Classes and Objects - Exercise/03/project/song.py
@@ -4,7 +4,7 @@ class Song:
     def __init__(self, name, length, single) -> None:
         self.name = name
         self.length = length
-        self.single = False
+        self.single = bool
 
     def get_info(self):
         return f"{self.name} - {self.length}"
