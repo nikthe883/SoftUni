8d53550264e5cf5a8d6e679de4d4e152772a4513
nikthe883 <popmihaylov@gmail.com>
2024-03-04 09:00:47 +0200

    update

---
+++ Python OOP/Static and Class Methods - Lab/calculator.py
@@ -0,0 +1,35 @@
+class Calculator:
+
+    @staticmethod
+    def add(*args):
+        result = 0
+        for n in args:
+            result += n
+        return result
+
+    @staticmethod
+    def multiply(*args):
+        result = 1
+        for n in args:
+            result *= n
+        return result
+
+    @staticmethod
+    def divide(*args):
+        
+        return args[0] / args[1]
+        
+        
+
+    @staticmethod
+    def subtract(*args):
+        result = args[0]
+        for n in args[1:]:
+            result -= n
+        return result
+
+
+print(Calculator.add(5, 10, 4))
+print(Calculator.multiply(1, 2, 3, 5))
+print(Calculator.divide(100, 2))
+print(Calculator.subtract(90, 20, -50, 43, 7))
